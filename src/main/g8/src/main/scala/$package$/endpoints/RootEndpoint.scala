a070f0587d9ce29f017012c495a2b223bdead243
VAlux <olex3000sp@gmail.com>
2021-01-29 11:34:06 +0200

    selection of basic or oauth authentication added

--- src/main/g8/src/main/scala/$package$/endpoints/RootEndpoint.scala
+++ src/main/g8/src/main/scala/$package$/endpoints/RootEndpoint.scala
@@ -1,15 +1,22 @@
 package $package$.endpoints
 
 import $package$.model.User
+import $package$.model.authentication.AuthenticationServiceDescriptor
+$if(add-oauth-authentication.truthy)$
 import $package$.model.authentication.JwtAuthenticationServiceDescriptor
+$endif$
+$if(add-basic-authentication.truthy)$
+import $package$.model.authentication.BasicAuthenticationServiceDescriptor
+$endif$
 import $package$.model.response._
-import $package$.service.authentication.JwtAuthenticationService
+import $package$.service.authentication.AuthenticationService
 import io.circe.generic.auto._
 import sttp.model.StatusCode
 import sttp.tapir.generic.auto._
 import sttp.tapir.json.circe.jsonBody
 import sttp.tapir.server.PartialServerEndpoint
 import sttp.tapir.{endpoint, _}
+import sttp.tapir.model.UsernamePassword
 
 object RootEndpoint {
 
@@ -19,6 +26,23 @@ object RootEndpoint {
       .name("$name$ API")
       .in("api" / "v1")
       .errorOut(errorResponseMapping)
+
+  $if(add-basic-authentication.truthy || add-oauth-authentication.truthy)$
+  def secureRootV1[F[_]](
+    authenticationService: AuthenticationService[F, AuthenticationServiceDescriptor]
+  ): PartialServerEndpoint[User, Unit, ErrorResponse, Unit, Any, F] =
+    rootV1
+    $if(add-oauth-authentication.truthy)$
+      .in(auth.bearer[String])
+      .serverLogicForCurrent(token => 
+        authenticationService.authenticate(JwtAuthenticationServiceDescriptor(token)))
+    $elseif(add-basic-authentication.truthy)$
+      .in(auth.basic[UsernamePassword])
+      .serverLogicForCurrent(usernamePassword => 
+        authenticationService.authenticate(BasicAuthenticationServiceDescriptor(usernamePassword)))
+    $endif$
+  $endif$
+
 
   private lazy val errorResponseMapping: EndpointOutput.OneOf[ErrorResponse, ErrorResponse] =
     oneOf[ErrorResponse](
--- src/main/g8/src/main/scala/$package$/model/authentication/AuthenticationServiceDescriptor.scala
+++ src/main/g8/src/main/scala/$package$/model/authentication/AuthenticationServiceDescriptor.scala
@@ -2,7 +2,11 @@ package $package$.model.authentication
 
 sealed trait AuthenticationServiceDescriptor
 
+$if(add-basic-authentication.truthy)$
 case class BasicAuthenticationServiceDescriptor(username: String, password: String)
     extends AuthenticationServiceDescriptor
+$endif$
 
+$if(add-oauth-authentication.truthy)$
 case class JwtAuthenticationServiceDescriptor(token: String) extends AuthenticationServiceDescriptor
+$endif$
